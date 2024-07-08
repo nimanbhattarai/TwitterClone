c5d3ae07f65288fd84dbc378a671c3141bf9d076
Niman Bhattarai <nimanbhattarai1234@gmail.com>
2024-07-08 12:51:23 +0545

    Uploded

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*html"],
+  theme: {
+    extend: {
+      colors:{
+        customGrey:'rgb(24 24 24)',
+        customBlue:'rgb(10 23 31)',
+        customPink:'#200914',
+        customGrey:'#080808'
+      }
+    },
+  },
+  plugins: [],
+}
+
